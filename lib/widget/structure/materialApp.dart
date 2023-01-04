72c54a7e7d76538fdd177f4ef7a1b753f9c17540
webabcd <webabcd@hotmail.com>
2023-01-04 09:36:22 +0800

    update

--- lib/widget/structure/materialApp.dart
+++ lib/widget/structure/materialApp.dart
@@ -9,12 +9,24 @@ class MaterialAppDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 50,
-        height: 50,
-        color: Colors.red,
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        // #docregion centered-text
+        body: const Center(
+          // #docregion text
+          child: Text('Hello World'),
+          // #enddocregion text
+        ),
+        // #enddocregion centered-text
       ),
     );
   }
 }
+
+
+
+// https://docs.flutter.dev/development/ui/layout#material-apps    materialapp  nonmaterialapp 他们继承的是什么  Material组件
