1938c154f3ba64362c2c193aae2611ce33705b1d
webabcd <webabcd@hotmail.com>
2023-01-04 09:33:01 +0800

    update

--- lib/widget/structure/scaffold_appBar.dart
+++ lib/widget/structure/scaffold_appBar.dart
@@ -1,5 +1,7 @@
 /*
+ * AppBar - 顶部导航栏
  *
+ * 一般在 Scaffold 中使用 AppBar
  */
 
 import 'package:flutter/material.dart';
@@ -20,10 +22,11 @@ class _ScaffoldAppBarDemoState extends State<ScaffoldAppBarDemo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: true,
-        /// leading: const Icon(Icons.arrow_back),
-        title: const Text('title'),
-        actions: <Widget>[
+        automaticallyImplyLeading: true,                  /// 当没有指定 leading 时，是否自动显示左侧的返回按钮并自动实现返回按钮的逻辑
+        /// leading: const Icon(Icons.arrow_back_ios),    /// 左侧按钮
+        /// leadingWidth: 100,                            /// 左侧按钮占位的宽度
+        title: const Text('title'),                       /// title
+        actions: <Widget>[                                /// 右侧的按钮集合
           IconButton(
             icon: const Icon(Icons.favorite),
             onPressed: () { },
@@ -33,7 +36,7 @@ class _ScaffoldAppBarDemoState extends State<ScaffoldAppBarDemo> {
             onPressed: () { },
           ),
         ],
-        flexibleSpace: const Icon(Icons.wifi),
+        flexibleSpace: FlexibleSpaceBar(title: MyText("flexibleSpace"),),     ///
         bottom: AppBar(
           automaticallyImplyLeading: false,
           title: const Text('bottom'),
@@ -42,7 +45,7 @@ class _ScaffoldAppBarDemoState extends State<ScaffoldAppBarDemo> {
             side: const BorderSide(width: 2, color: Colors.red),
             borderRadius: BorderRadius.circular(0)
         ),
-
+// http://events.jianshu.io/p/20851a96b92f
         backgroundColor: Colors.green,
         shadowColor: Colors.black,
         elevation: 100,
@@ -53,8 +56,8 @@ class _ScaffoldAppBarDemoState extends State<ScaffoldAppBarDemo> {
         titleSpacing: 20,
         toolbarOpacity: 1,
         bottomOpacity: 1,
-        toolbarHeight: 100,
-        leadingWidth: 100,
+        //toolbarHeight: 100,
+
 
 
         toolbarTextStyle: TextStyle(color: Colors.purple),
