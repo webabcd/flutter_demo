63d92e7b0ccfe3e6c1e0066ea5c04faf98d8671f
webabcd <webabcd@hotmail.com>
2023-04-18 09:12:17 +0800

    update

--- lib/input/listener.dart
+++ lib/input/listener.dart
@@ -27,29 +27,109 @@ class _ListenerDemoState extends State<ListenerDemo> {
     return Scaffold(
       appBar: null,
       body: Center(
-        /// 通过 Listener 监听指针事件
-        child: Listener(
-          child: Container(
-            color: Colors.red,
-            width: 300,
-            height: 300,
-          ),
-          /// 按下
-          onPointerDown: (event) { /// event 是一个 PointerDownEvent 对象
-            /// position - 相对于屏幕的位置
-            /// localPosition - 相对于父的位置
-            log('onPointerDown: ${event.position}, ${event.localPosition}');
-          },
-          /// 按下后移动
-          onPointerMove: (event) { /// event 是一个 PointerMoveEvent 对象
-            /// delta - 两次事件之间的 position 的差值
-            /// localDelta - 两次事件之间的 localPosition 的差值
-            log('onPointerMove: ${event.position}, ${event.delta}, ${event.localPosition}, ${event.localDelta}');
-          },
-          /// 抬起
-          onPointerUp: (event) { /// event 是一个 PointerUpEvent 对象
-            log('onPointerUp: ${event.position}, ${event.localPosition}');
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            /// 通过 Listener 监听指针事件
+            Listener(
+              child: Container(
+                color: Colors.red,
+                width: 200,
+                height: 100,
+              ),
+              /// 按下
+              onPointerDown: (event) { /// event 是一个 PointerDownEvent 对象
+                /// position - 相对于屏幕的位置
+                /// localPosition - 相对于父的位置
+                log('onPointerDown: ${event.position}, ${event.localPosition}');
+              },
+              /// 按下后移动
+              onPointerMove: (event) { /// event 是一个 PointerMoveEvent 对象
+                /// delta - 两次事件之间的 position 的差值
+                /// localDelta - 两次事件之间的 localPosition 的差值
+                log('onPointerMove: ${event.position}, ${event.delta}, ${event.localPosition}, ${event.localDelta}');
+              },
+              /// 抬起
+              onPointerUp: (event) { /// event 是一个 PointerUpEvent 对象
+                log('onPointerUp: ${event.position}, ${event.localPosition}');
+              },
+            ),
+
+            /// 用于演示指针事件的冒泡
+            Listener(
+              child: Container(
+                color: Colors.red,
+                width: 200,
+                height: 100,
+                alignment: Alignment.center,
+                child:Listener(
+                  child: Container(
+                    color: Colors.green,
+                    width: 100,
+                    height: 50,
+                  ),
+                  onPointerDown: (event) {
+                    log('onPointerDown green: ${event.position}, ${event.localPosition}');
+                  },
+                ),
+              ),
+              onPointerDown: (event) {
+                log('onPointerDown red: ${event.position}, ${event.localPosition}');
+              },
+            ),
+
+            Listener(
+              child: Container(
+                color: Colors.red,
+                width: 200,
+                height: 100,
+                alignment: Alignment.center,
+                /// 禁止响应指针事件，但是会冒泡
+                child:IgnorePointer(
+                  ignoring: true,
+                  child: Listener(
+                    child: Container(
+                      color: Colors.green,
+                      width: 100,
+                      height: 50,
+                    ),
+                    onPointerDown: (event) {
+                      log('onPointerDown green: ${event.position}, ${event.localPosition}');
+                    },
+                  ),
+                ),
+              ),
+              onPointerDown: (event) {
+                log('onPointerDown red: ${event.position}, ${event.localPosition}');
+              },
+            ),
+
+            Listener(
+              child: Container(
+                color: Colors.red,
+                width: 200,
+                height: 100,
+                alignment: Alignment.center,
+                /// 禁止响应指针事件，并且不会冒泡
+                child:AbsorbPointer(
+                  absorbing: true,
+                  child: Listener(
+                    child: Container(
+                      color: Colors.green,
+                      width: 100,
+                      height: 50,
+                    ),
+                    onPointerDown: (event) {
+                      log('onPointerDown green: ${event.position}, ${event.localPosition}');
+                    },
+                  ),
+                ),
+              ),
+              onPointerDown: (event) {
+                log('onPointerDown red: ${event.position}, ${event.localPosition}');
+              },
+            ),
+          ],
         ),
       ),
     );
