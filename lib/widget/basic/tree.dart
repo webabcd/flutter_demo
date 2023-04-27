b42537ed3b2a8859a80be58622ab7f0dc5c58f58
webabcd <webabcd@hotmail.com>
2023-04-27 12:31:36 +0800

    update

--- lib/widget/basic/tree.dart
+++ lib/widget/basic/tree.dart
@@ -12,7 +12,7 @@
  *
  * 首次新建某个 Widget 对象时，会自动新建一个持有此 Widget 的 Element 对象
  * 当这个 View 需要更新时，需要重新生成这个 Widget 对象，这个新生成的 Widget 对象会替代之前的 Widget 对象与 Element 建立关系
- * 这就是所谓的 Element 与 Widget 的一对多关系
+ * 这就是所谓的 Element 与 Widget 的一对多关系，但是同一时刻 Element 与 Widget 其实是一对一的关系
  */
 
 import 'package:flutter/material.dart';
@@ -38,6 +38,19 @@ class _TreeDemoState extends State<TreeDemo> {
     _MyStatefulWidget(color: Colors.green, type: 1, key: UniqueKey(),),
   ];
 
+  /// 颜色信息保存在 Widget 的 State 中
+  List<Widget> widgets3 = [
+    _MyStatefulWidget(color: Colors.red, type: 1),
+    _MyStatefulWidget(color: Colors.green, type: 1),
+  ];
+
+  /// 颜色信息保存在 Widget 的 State 中
+  List<Widget> widgets4 = [
+    _MyStatefulWidget(color: Colors.red, type: 1, key: UniqueKey()),
+    _MyStatefulWidget(color: Colors.green, type: 1, key: UniqueKey()),
+    _MyStatefulWidget(color: Colors.blue, type: 1, key: UniqueKey()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -45,16 +58,17 @@ class _TreeDemoState extends State<TreeDemo> {
       children: [
         GestureDetector(
           child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: widgets1
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: widgets1,
           ),
           onTap: () {
             /// 交换两个 Widget 的位置，颜色信息保存在 Widget 中
             /// 在 Widget 树中，两个 Widget 的位置对调了
-            /// 此时 Element 树没有变化，每个 Element 持有的 Widget 的地址也没有变化
-            /// 但是因为两个 Widget 对调了，所以第 1 个 Element 持有了原来第 2 个 Widget，第 2 个 Element 持有了原来第 1 个 Widget
+            /// 此时 Element 树没有变化，每个 Element 通过 Widget 树中 Widget 的索引位置持有其对应的 Widget
+            /// 也就是说，第 1 个 Element 持有的是 Widget 树上的第 1 个 Widget，第 2 个 Element 持有的是 Widget 树上的第 2 个 Widget
+            /// 因为两个 Widget 对调了，所以第 1 个 Element 持有了原来第 2 个 Widget，第 2 个 Element 持有了原来第 1 个 Widget
             /// Element 会调用持有的 Widget 的 canUpdate() 方法，其会判断新老 Widget 的 key 和 runtimeType 是否相同（注：如果没指定 Widget 的 key 则认为 key 是相同的）
-            /// 然后发现 key 和 runtimeType 均相同，所以第 1 个 Element 就认为其持有原来的第 2 个 Widget 是合法的
+            /// 然后发现 key 和 runtimeType 均相同，所以第 1 个 Element 就认为其持有原来的第 2 个 Widget 是合法的，同理第 2 个 Element 就认为其持有原来的第 1 个 Widget 是合法的
             /// 因为使用的颜色信息是保存在 Widget 中的，所以随着 Widget 的对调，颜色也对调了
             widgets1.insert(0, widgets1.removeAt(1));
             setState(() {});
@@ -62,24 +76,91 @@ class _TreeDemoState extends State<TreeDemo> {
         ),
         GestureDetector(
           child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: widgets2
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: widgets2,
           ),
           onTap: () {
             /// 交换两个 Widget 的位置，颜色信息保存在 Widget 的 State 中（注：State 是保存在 Element 树中的）
             /// 在 Widget 树中，两个 Widget 的位置对调了
-            /// 此时 Element 树没有变化，每个 Element 持有的 Widget 的地址也没有变化
-            /// 但是因为两个 Widget 对调了，所以第 1 个 Element 持有了原来第 2 个 Widget，第 2 个 Element 持有了原来第 1 个 Widget
+            /// 此时 Element 树没有变化，每个 Element 通过 Widget 树中 Widget 的索引位置持有其对应的 Widget
+            /// 也就是说，第 1 个 Element 持有的是 Widget 树上的第 1 个 Widget，第 2 个 Element 持有的是 Widget 树上的第 2 个 Widget
+            /// 因为两个 Widget 对调了，所以第 1 个 Element 持有了原来第 2 个 Widget，第 2 个 Element 持有了原来第 1 个 Widget
             /// Element 会调用持有的 Widget 的 canUpdate() 方法，其会判断新老 Widget 的 key 和 runtimeType 是否相同（注：如果没指定 Widget 的 key 则认为 key 是相同的）
-            ///   1、如果你没有为 Widget 指定不同的 key，则 key 和 runtimeType 均相同，所以第 1 个 Element 就认为其持有原来的第 2 个 Widget 是合法的
+            ///   1、如果你没有为 Widget 指定不同的 key，则 key 和 runtimeType 均相同，所以第 1 个 Element 就认为其持有原来的第 2 个 Widget 是合法的，同理第 2 个 Element 就认为其持有原来的第 1 个 Widget 是合法的
             ///   因为使用的颜色信息是保存在 Widget 的 State 中的（State 是保存在 Element 树中的），所以虽然 Widget 对调了，但是颜色不会对调
             ///   2、如果你为 Widget 指定了不同的 key，则因为 key 是不同的，所以第 1 个 Element 就认为其持有原来的第 2 个 Widget 是不合法的
-            ///   然后 Element 就会在 Widget 树种查找，看看有没有和之前的 Widget 的 key 和 runtimeType 都相同的
-            ///   结果找到了，然后第 1 个 Element 就持有了原来的第 1 个 Widget，所以随着 Widget 的对调，颜色也对调了
+            ///   然后第 1 个 Element 就会在原来的第 2 个 Widget 的同级树上查找（不会在父级或子级查找），看看有没有和之前的 Widget 的 key 和 runtimeType 都相同的
+            ///   结果找到了，然后第 1 个 Element 就持有了原来的第 1 个 Widget，同理第 2 个 Element 就持有了原来的第 2 个 Widget
+            ///   所以随着 Widget 的对调，颜色也对调了
             widgets2.insert(0, widgets2.removeAt(1));
             setState(() {});
           },
         ),
+        GestureDetector(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: widgets3,
+          ),
+          onTap: () {
+            /// 把上面的都看懂了，再来理解这个
+            /// 从位置 0 插入蓝色的 Widget，颜色信息保存在 Widget 的 State 中（注：State 是保存在 Element 树中的）
+            /// 开始时，红色的 widget1，绿色的 widget2，element1 持有 widget1，element2 持有 widget2
+            /// 第一次在 0 位置插入蓝色的 widget3 后
+            ///   1、没有为 Widget 指定不同的 key 时
+            ///   element1 持有 widget3，显示为红色（element1 持有的是 widget 树中的第 1 个 widget）
+            ///   element2 持有 widget1，显示为绿色（element2 持有的是 widget 树中的第 2 个 widget）
+            ///   widget2 没人要，所以会新建一个 element3 持有之，显示为绿色（element3 持有的是 widget 树中的第 3 个 widget）
+            ///   显示结果：红绿变为红绿绿
+            ///   2、为 Widget 指定不同的 key 时
+            ///   element1 持有 widget1，显示为红色（element1 持有的是 widget 树中的第 2 个 widget）
+            ///   element2 持有 widget2，显示为绿色（element2 持有的是 widget 树中的第 3 个 widget）
+            ///   widget3 没人要，所以会新建一个 element3 持有之，显示为蓝色（element3 持有的是 widget 树中的第 1 个 widget）
+            ///   显示结果：红绿变为蓝红绿
+            widgets3.insert(0, _MyStatefulWidget(color: Colors.blue, type: 1));
+            setState(() {});
+          },
+        ),
+        GestureDetector(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: widgets4,
+          ),
+          onTap: () {
+            /// 把上面的都看懂了，再来理解这个
+            /// 从位置 0 删除 Widget，颜色信息保存在 Widget 的 State 中（注：State 是保存在 Element 树中的）
+            /// 开始时，红色的 widget1，绿色的 widget2，蓝色的 widget3，element1 持有 widget1，element2 持有 widget2，element3 持有 widget3
+            /// 第一次在 0 位置删除红色的 widget1 后
+            ///   1、没有为 Widget 指定不同的 key 时
+            ///   element1 持有 widget2，显示为红色（element1 持有的是 widget 树中的第 1 个 widget）
+            ///   element2 持有 widget3，显示为绿色（element2 持有的是 widget 树中的第 2 个 widget）
+            ///   element3 找不到对应的 Widget 所以 element3 会被销毁
+            ///   显示结果：红绿蓝变为红绿
+            ///   2、为 Widget 指定不同的 key 时
+            ///   element2 持有 widget2，显示为绿色（element2 持有的是 widget 树中的第 1 个 widget）
+            ///   element3 持有 widget3，显示为蓝色（element3 持有的是 widget 树中的第 2 个 widget）
+            ///   element1 找不到对应的 Widget 所以 element1 会被销毁
+            ///   显示结果：红绿蓝变为绿蓝
+            widgets4.removeAt(0);
+            setState(() {});
+          },
+        ),
+
+        /// 把上面的示例都看懂后，再加深一遍理解，比如初始状态如下
+        /// Widget树                    Element树
+        /// root                        root
+        /// |--w1                       |--e1
+        /// |  |--w1_1                  |  |--e1_1
+        /// |  |--w1_2                  |  |--e1_2
+        /// |  |  |--w1_2_1             |  |  |--e1_2_1
+        /// |  |  |--w1_2_2             |  |  |--e1_2_2
+        /// |  |  |--w1_2_3             |  |  |--e1_2_3
+        /// |  |--w1_3                  |  |--e1_3
+        /// |--w2                       |--e2
+        /// 上面 e1_2_1 持有 w1_2_1，e1_2_2 持有 w1_2_2，e1_2_3 持有 w1_2_3
+        /// 当 w1_2_1/w1_2_2/w1_2_3 这一级发生变化时，e1_2_1/e1_2_2/e1_2_3 查找其持有的 widget 时只会在同级树上查找（不会在父级或子级查找）
+        /// 查找时，会先按照之前的索引位置查找
+        /// 比如 e1_2_1 之前持有的是同级 widget 树上的第 1 个 widget，则 widget 树发生变化后，e1_2_1 会先去找同级 widget 树上的第 1 个 widget
+        /// 也就是说如果 w1_2_1/w1_2_2/w1_2_3 类型相同且没有指定不同的 key，则删除 w1_2_1 后 e1_2_1 会持有 w1_2_2
 
         /// 通过 Builder 获取上下文，这个上下文是由框架传递过来的
         Builder(
