0f075cadef348a442b370ae84bb38d3d28b87d14
webabcd <webabcd@hotmail.com>
2023-05-05 10:44:59 +0800

    update

--- lib/widget/layout/flex.dart
+++ lib/widget/layout/flex.dart
@@ -8,7 +8,7 @@
  *
  * Row 和 Column 都继承自 Flex，大家的用法都是差不多的，本例主要演示 Flex 中弹性布局 Expanded, Flexible, Spacer 的用法
  * 在 Row 或 Column 中使用 Expanded, Flexible, Spacer 请参考本例即可
- * 实际开发中，建议能用 Row 或 Column 就别用 Flex
+ * 实际开发中，建议能用 Row 或 Column 就别用 Flex（但是如果需求是，能在水平弹性布局和垂直弹性布局之间随其切换，则用 Flex 比较好）
  *
  * 注：Expanded, Flexible, Spacer 只能应用于类似 Flex/Row/Column 这种弹性布局控件
  */
