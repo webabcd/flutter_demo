79b098b5061436b623a188d163227bc906499d25
webabcd <webabcd@hotmail.com>
2023-05-06 11:24:47 +0800

    update

--- lib/widget/container/material.dart
+++ lib/widget/container/material.dart
@@ -1,8 +1,7 @@
 /*
  * Material - Material 组件
  *
- * Material 组件就是符合 Material Design 的一套组件，比如 Scaffold, TextButton 等
- * 而 Material 就是最简单的 Material 组件
+ * Material 组件是符合 Material Design 规范的最简单的组件
  */
 
 import 'package:flutter/material.dart';
@@ -12,22 +11,69 @@ class MaterialDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// 因为 IconButton 之类的必须放到一个 Material 组件内，否则会报错
+    return Scaffold(
+      appBar: AppBar(title: const Text("title"),),
+      backgroundColor: Colors.orange,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          /// Material - 最轻量级的符合 Material Design 规范的组件
+          Material(
+            /// 阴影颜色
+            shadowColor: Colors.green,
+            /// 阴影大小
+            elevation: 10,
+            /// 背景颜色
+            color: Colors.red,
+            /// 边框
+            shape: OutlineInputBorder(
+              borderSide: const BorderSide(width: 1, color: Colors.green),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            /// 子
+            child: const Icon(
+              Icons.home,
+              size: 72,
+              color: Colors.white,
+            ),
+          ),
+
+          const Material(
+            /// 圆形的 Material 组件（注：如果设置了 type 为 MaterialType.circle 则必须设置 color）
+            type: MaterialType.circle,
+            color: Colors.red,
+            child: Icon(
+              Icons.home,
+              size: 72,
+              color: Colors.white,
+            ),
+          ),
+
+          Material(
+            /// 背景透明的 Material 组件，可以在其中使用 Ink 相关组件
+            type: MaterialType.transparency,
+            child: InkWell(
+              onTap: () { },
+              child: const Icon(
+                Icons.home,
+                size: 72,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    /// 另外，有一些组件，比如 IconButton 之类的必须放到一个 Material 组件内，否则会报错
     /// 可以用 Scaffold 之类的，也可以用最轻量级的 Material
     return Material(
-      /// 颜色
-      color: Colors.orange,
-      /// 边框
-      shape: const OutlineInputBorder(
-        borderSide: BorderSide(width: 20, color: Colors.green),
-      ),
-      /// 子
       child: IconButton(
         onPressed: () { },
         icon: const Icon(
           Icons.home,
           size: 72,
-          color: Colors.white,
+          color: Colors.red,
         ),
       ),
     );
